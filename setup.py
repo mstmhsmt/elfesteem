0ded1ca9529bbcc0f9b87f1c8f1916c4898c5906
mstmhsmt <m.hashimoto@stair.center>
2026-05-08 11:03:10 +0900

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = 'ELFEsteem',
-    version = '0.2.1-aies0',
+    version = '0.2.1+aies0',
     packages=['elfesteem'],
     scripts = ['elfcli'],
     # Metadata
